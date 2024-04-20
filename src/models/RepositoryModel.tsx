a2380b20a6f09ea7c887194437f3370f8a54e692
sysout98 <samuele.varianti@gmail.com>
2024-04-20 21:07:41 +0200

    feat: Improve Repository model and define stargazer model

--- src/models/RepositoryModel.tsx
+++ src/models/RepositoryModel.tsx
@@ -4,5 +4,9 @@ export type Repository = {
     description: string;
     stargazers_url: string;
     stargazers_count: string;
+    full_name: string;
+    owner: {
+        login: string;
+    }
 }
 
---
+++ src/models/StargazerModel.tsx
@@ -0,0 +1,5 @@
+export type Stargazer = {
+    "avatar_url": string;
+    "url": string;
+    login: string;
+}
