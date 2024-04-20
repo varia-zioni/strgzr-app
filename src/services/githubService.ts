13ee0283d5f265fdb34fa58bb240e64f22a7a906
sysout98 <samuele.varianti@gmail.com>
2024-04-21 00:27:03 +0200

    feat: Improve githubService methods

--- src/services/githubService.ts
+++ src/services/githubService.ts
@@ -1,18 +1,38 @@
 import Config from "react-native-config";
-import { Repository } from "../models/RepositoryModel";
 
-export function fetchUserRepositories({ username, pageLimit, page }: { username: string; pageLimit: number; page: number; }): Promise<Array<Repository>> {
+export function fetchUserRepositories({ username, pageLimit, page }: { username: string; pageLimit: number; page: number; }) {
     return fetch(
-        `https://api.github.com/users/${username}}/repos?per_page=${pageLimit}&page=${page}`,
+        `https://api.github.com/users/${username}/repos?per_page=${pageLimit}&page=${page}`,
         {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': `Beared ${Config.GIT_HUB_TOKEN}`
+                'Authorization': `Bearer ${Config.GIT_HUB_TOKEN}`
             }
-        })
-        .then(async (data) => {
-            return data.json();
-        })
-        .catch((_) => Promise.resolve([]));
+        });
+}
+
+export function fetchFilteredRepositories({ username, repoFilter, page, pageLimit }: { username: string; repoFilter: string; page: number; pageLimit: number;}) {
+    return fetch(
+        `https://api.github.com/search/repositories?per_page=${pageLimit}&page=${page}&q=${encodeURIComponent(`${repoFilter} in:name user:${username}`)}`,
+        {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${Config.GIT_HUB_TOKEN}`
+            }
+        });
+}
+
+export function fetchRepositoryStargazers({ username, repoName }: { username: string; repoName: string; }) {
+    return  fetch(
+        `https://api.github.com/repos/${username}/${repoName}/stargazers?per_page=100`,
+        {
+            method: 'GET',
+            headers: { 
+                'Content-Type': 'application/json',
+                'Authorization':  `Bearer ${Config.GIT_HUB_TOKEN}`
+             }
+
+        });
 }
