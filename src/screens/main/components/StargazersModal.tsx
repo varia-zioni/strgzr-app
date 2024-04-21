0d87429aa5ecc331d4021841f71a79f92661098f
sysout98 <samuele.varianti@gmail.com>
2024-04-21 21:46:04 +0200

    fix: Overflow modal text & empty state

--- src/screens/main/components/StargazersModal.tsx
+++ src/screens/main/components/StargazersModal.tsx
@@ -69,11 +69,11 @@ export default function StargazersModal({ openModal, setOpenModal, repo }: Props
             <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
                 <Card style={{ marginHorizontal: 20, backgroundColor: styles.colors.lightPurple, maxHeight: 700, width: 350 }}>
                     <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "space-between", paddingVertical: 15, paddingLeft: 15 }}>
-                        <View style={{ flexDirection: "row", alignItems: "center" }}>
+                        <View style={{ flexDirection: "row", alignItems: "center", flex: 0.9 }}>
                             <Icon source="github" size={25} color={styles.colors.black} />
-                            <Text variant="titleMedium" style={{ color: styles.colors.black, marginLeft: 15, paddingRight: 20 }} >{repo.name}</Text>
+                            <Text variant="titleMedium" style={{ color: styles.colors.black, marginLeft: 15, marginRight: 15 }} >{repo.name}</Text>
                         </View>
-                        <IconButton icon="close" iconColor={styles.colors.black} size={25} onPress={() => setOpenModal(false)} />
+                        <IconButton style={{ flex: 0.1 }} icon="close" iconColor={styles.colors.black} size={25} onPress={() => setOpenModal(false)} />
                     </View>
                     <Divider bold />
                     <View style={{ flexDirection: "row", justifyContent: "center", padding: 15 }}>
@@ -89,12 +89,13 @@ export default function StargazersModal({ openModal, setOpenModal, repo }: Props
                                     onEndReached={() => getStargazers(page + 1)}
                                     onEndReachedThreshold={20}
                                     ListEmptyComponent={() => (
-                                        <>
+                                        <View style={{ alignItems: "center" }}>
+                                            <Icon source="emoticon-sad-outline" size={30} color={styles.colors.black} />
                                             <Text variant="bodyLarge" style={{ color: styles.colors.black, marginRight: 10 }}>
                                                 Non sono presenti stargazers
                                             </Text>
-                                            <Icon source="emoticon-sad-outline" size={30} color={styles.colors.black} />
-                                        </>
+
+                                        </View>
                                     )}
                                     refreshControl={
                                         <RefreshControl refreshing={nextPageLoading} colors={[styles.colors.strongPurple]} />
